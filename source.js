3ce8e82b540bc365f9f811a35190c85448fcea89
tmanthegamer <tmanthegamer@gmail.com>
2017-04-16 21:54:40 -0700

    initial commit, should look into react js, might be fun.

---
+++ source.js
@@ -0,0 +1,38 @@
+console.log('hello');
+
+$( document ).ready(function() {
+    
+    // var MAX_HEIGHT  = $(window).height();
+    // var MAX_WIDTH   = $(window).width();
+});
+
+
+// $( window ).load(function() {
+//     console.log('hello');
+//     //Create the audio tag
+//     var soundFile = document.createElement("audio");
+//     soundFile.preload = "auto";
+
+//     //Load the sound file (using a source element for expandability)
+//     var src = document.createElement("source");
+//     src.src = "Snd_1.mp3";
+//     soundFile.appendChild(src);
+
+//     //Load the audio tag
+//     //It auto plays as a fallback
+//     soundFile.load();
+//     soundFile.volume = 0.000000;
+//     soundFile.play();
+
+//     //Plays the sound
+//     function play() {
+//        //Set the current time for the audio file to the beginning
+//        soundFile.currentTime = 0.01;
+//        soundFile.volume = volume;
+
+//        //Due to a bug in Firefox, the audio needs to be played after a delay
+//        setTimeout(function(){soundFile.play();},1);
+//     }
+
+//     play();
+// });
